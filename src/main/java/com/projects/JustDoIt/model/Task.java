d1bbf142d3754f58994a17c837a49642b54e8bbd
Atif Pathan <atifkhan.p1@gmail.com>
2024-06-23 01:34:47 -0400

    added Lombok dependency and related annotations

--- src/main/java/com/projects/JustDoIt/model/Task.java
+++ src/main/java/com/projects/JustDoIt/model/Task.java
@@ -1,8 +1,18 @@
 package com.projects.JustDoIt.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Entity
 @Table(name = "Tasks")
 public class Task {
@@ -13,64 +23,65 @@ public class Task {
 
     @NotEmpty
     private String title;
+
     private String description;
     private Boolean finished;
 
-    /**
-     * Default Constructor
-     */
-    protected Task() {
-
-    }
-
-    /**
-     * @param title is the title of the specific task
-     * @param description is a short description of what the task is or extra info needed to complete the task
-     * @param finished is a flag to keep track of if the task is complete or not
-     */
-    public Task(String title, String description, Boolean finished) {
-        this.title = title;
-        this.description = description;
-        this.finished = finished;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Boolean isFinished() {
-        return finished;
-    }
-
-    public void setFinished(Boolean finished) {
-        this.finished = finished;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "Task %d: [Title: %s; Description: %s; Status: %b]",
-                id, title, description, finished);
-    }
+//    /**
+//     * Default Constructor
+//     */
+//    protected Task() {
+//
+//    }
+//
+//    /**
+//     * @param title is the title of the specific task
+//     * @param description is a short description of what the task is or extra info needed to complete the task
+//     * @param finished is a flag to keep track of if the task is complete or not
+//     */
+//    public Task(String title, String description, Boolean finished) {
+//        this.title = title;
+//        this.description = description;
+//        this.finished = finished;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public Boolean isFinished() {
+//        return finished;
+//    }
+//
+//    public void setFinished(Boolean finished) {
+//        this.finished = finished;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return String.format(
+//                "Task %d: [Title: %s; Description: %s; Status: %b]",
+//                id, title, description, finished);
+//    }
 
 }
